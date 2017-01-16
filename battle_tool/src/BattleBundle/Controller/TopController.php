0919574fdbdea320afc9da58ca3bac2e4ef66573
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2017-01-16 10:55:06 +0900

    routeをannotation方式に変更

--- battle_tool/src/BattleBundle/Controller/TopController.php
+++ battle_tool/src/BattleBundle/Controller/TopController.php
@@ -3,24 +3,37 @@
 namespace BattleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class TopController extends Controller
 {
+    /**
+     *@Route("/")
+     */
     public function indexAction()
     {
         return $this->render('BattleBundle:Top:index.html.twig');
     }
 
+    /**
+     *@Route("/user/{id}")
+     */
     public function userTopAction()
     {
         return $this->render('BattleBundle:Top:user_top.html.twig');
     }
 
+    /**
+     *@Route("/party/new")
+     */
     public function partyNewAction()
     {
         return $this->render('BattleBundle:Top:party_new.html.twig');
     }
 
+    /**
+     *@Route("/battle")
+     */
     public function battleAction()
     {
         return $this->render('BattleBundle:Top:battle.html.twig');
